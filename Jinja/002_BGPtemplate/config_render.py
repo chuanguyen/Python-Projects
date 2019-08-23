62c724e4642e7f1ed8df0c9256ff743fcfab9f43
Calvin Hua <chua@dwavesys.com>
2019-08-23 11:45:38 -0700

    Updated BGP Jinja template

--- Jinja/002_BGPtemplate/config_render.py
+++ Jinja/002_BGPtemplate/config_render.py
@@ -1,23 +1,43 @@
 import yaml
 from jinja2 import Template
+from argparse import ArgumentParser
 
-data_file_name = "routerBGP-Data.yaml"
-template_file_name = "bgp_Template.j2"
+parser = ArgumentParser("Specifying the YAML File")
 
-with open(data_file_name) as f:
-    yaml_data = yaml.load(f)
+parser.add_argument("-d", "--data-file",
+                    help="Please specify the YAMl file.",
+                    required=True
+                   )
+parser.add_argument("-t", "--template-file",
+                    help="Please specify the Jinja template file.",
+                    required=True
+                   )
+args = parser.parse_args()
 
-with open(template_file_name) as f:
-    template_file = Template(f.read(), keep_trailing_newline=True)
+# data_file_name = "routerBGP-Data.yaml"
+# template_file_name = "bgp_Template.j2"
+data_file_name = args.data_file
+template_file_name = args.template_file
 
-for node in yaml_data:
+# Verifies whether the provided data and template file exist before continuing
+try:
+  with open(data_file_name) as f:
+      yaml_data = yaml.safe_load(f)
 
-  # Stores full configuration that will be exported to a file
-  generated_configs = template_file.render(rid=node['rid'],
-                                          local_asn=node['local_asn'],
-                                          neighbors=node['neighbors'],
-                                          networks=node['networks']
-                                         )
+  with open(template_file_name) as f:
+      template_file = Template(f.read(), keep_trailing_newline=True)
 
-  with open("{}_BGP-config.txt".format(node['hostname']), "w") as f:
-    f.write(generated_configs)
+  for node in yaml_data:
+
+    # Stores full configuration that will be exported to a file
+    generated_configs = template_file.render(rid=node['rid'],
+                                            local_asn=node['local_asn'],
+                                            neighbors=node['neighbors'],
+                                            networks=node['networks']
+                                           )
+
+    with open("{}_BGP-config.txt".format(node['hostname']), "w") as f:
+      f.write(generated_configs)
+
+except FileNotFoundError as e:
+  print(e)
