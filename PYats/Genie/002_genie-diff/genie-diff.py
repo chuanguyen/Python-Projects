d55de745d03b2202f85da06648d88003c8ecb02b
CalvinDawgz <calvindawgz1999@gmail.com>
2020-04-20 07:37:17 -0700

    Learned features through Genie from Python

--- PYats/Genie/002_genie-diff/genie-diff.py
+++ PYats/Genie/002_genie-diff/genie-diff.py
@@ -1,1 +1,40 @@
 #!/usr/bin/env python
+
+from genie.conf import Genie
+import pprint
+
+
+testbed = Genie.init("testbed.yaml")
+
+### Retrieving learned features
+
+baseline = {}
+
+for hostname,dev in testbed.devices.items():
+    dev.connect()
+    baseline[hostname] = {}
+    baseline[hostname]["interface"] = dev.learn("interface")
+    pprint.pprint(baseline[hostname]["interface"].info)
+
+
+# for device in testbed.devices:
+#     dev = testbed.devices[device]
+#     dev.connect()
+#     interfaces = dev.parse("show interfaces")
+#     interface_details[device] = interfaces
+#
+# interface_file = "interfaces.csv"
+#
+# with open (interface_file, "w") as f:
+
+
+
+### Retrieving learned features
+
+# baseline = {}
+#
+# for hostname,dev in testbed.devices.items():
+#     dev.connect()
+#     baseline[hostname] = {}
+#     baseline[hostname]["interface"] = dev.learn("interface")
+#     pprint.pprint(baseline[hostname]["interface"].info)
