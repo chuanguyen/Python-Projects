700e61483d0309654ec308913b056d44883bd84f
CalvinDawgz <calvindawgz1999@gmail.com>
2020-05-03 08:04:57 -0700

    Improved the table creation for tabulation

--- PYats/Genie/002_genie-Python/genie_learn-tabulate.py
+++ PYats/Genie/002_genie-Python/genie_learn-tabulate.py
@@ -6,29 +6,42 @@ from tabulate import tabulate
 from genie.conf import Genie
 
 testbed = Genie.init("testbed.yaml")
-devices_info = {}
-
-for device in testbed.devices:
-    dev = testbed.devices[device]
-    dev.connect(via='mgmt')
-    output = dev.learn('interface')
-    pprint.pprint(output.info)
-
-    devices_info[dev.name] = []
-
-    for intf_name,details in output.info.items():
-        if ("description" in details):
-           devices_info[dev.name].append((intf_name, details['enabled'],details['description'],details['bandwidth'],details['delay']))
-        else:
-           devices_info[dev.name].append((intf_name, details['enabled'],"",details['bandwidth'],details['delay']))
-
-print('\n' + '-'*80)
-
-for hostname,details in devices_info.items():
-    print(hostname)
-    print(tabulate(details, headers=["Interface Name","Enabled","Description","Bandwidth (Kbps)","Delay"])+"\n")
-
-print('-'*80 + '\n')
+# devices_info = {}
+#
+# for device in testbed.devices:
+#     dev = testbed.devices[device]
+#     dev.connect(via='mgmt')
+#     output = dev.learn('interface')
+#
+#     # Mega table containing interface info for each device
+#     devices_info[dev.name] = []
+#
+#     for intf_name,details in output.info.items():
+#         # Represents a row containing interface info
+#         tr = []
+#
+#         if ("description" in details):
+#            tr.append(intf_name)
+#            tr.append(details['enabled'])
+#            tr.append(details['description'])
+#            tr.append(details['bandwidth'])
+#            tr.append(details['delay'])
+#            devices_info[dev.name].append(tr)
+#         else:
+#            tr.append(intf_name)
+#            tr.append(details['enabled'])
+#            tr.append("")
+#            tr.append(details['bandwidth'])
+#            tr.append(details['delay'])
+#            devices_info[dev.name].append(tr)
+#
+# print('\n' + '-'*80)
+#
+# for hostname,details in devices_info.items():
+#     print(hostname)
+#     print(tabulate(details, headers=["Interface Name","Enabled","Description","Bandwidth (Kbps)","Delay"])+"\n")
+#
+# print('-'*80 + '\n')
 
 ### Retrieve version info
 # device_info = []
@@ -39,7 +52,11 @@ print('-'*80 + '\n')
 #     output = dev.parse("show version")
 #     platform_info = output['version']
 #
-#     device_info.append((dev.name,platform_info['os'],platform_info['version']))
+#     tr = []
+#     tr.append(dev.name)
+#     tr.append(platform_info['os'])
+#     tr.append(platform_info['version'])
+#     device_info.append(tr)
 #
 # print('\n' + '-'*80)
 #
