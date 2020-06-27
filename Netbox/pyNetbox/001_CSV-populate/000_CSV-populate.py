e76e2dfaed518a2ff72a5bf8f3c376dfe55582a3
Calvin Hua-Nguyen <calvindawgz1999@gmail.com>
2020-06-27 08:52:12 -0700

    Created CSV population pyNetbox script - Takes CSV of devices and creates them - Duplicate IP checking - Invalid model checking - Primary IP assignment to specific interface

--- Netbox/pyNetbox/001_CSV-populate/000_CSV-populate.py
+++ Netbox/pyNetbox/001_CSV-populate/000_CSV-populate.py
@@ -24,6 +24,9 @@ nb_source_file = "nb_devices.csv"
 nb_all_devices = list()
 nb_all_devices_primaryIPs = dict()
 
+# Stores IPs for duplicate checking
+all_IPs = list()
+
 # Stores non-existent NetBox objects defined in CSV
 nb_non_existent_count = 0
 nb_non_existent_objects = dict()
@@ -52,37 +55,48 @@ try:
               nb_non_existent_objects['device_type'].append(row['device_type'])
               nb_non_existent_count += 1
           if (not ndev_drole):
-              nb_non_existent_objects['device_role'].append(row['device_type'])
+              nb_non_existent_objects['device_role'].append(row['device_role'])
               nb_non_existent_count += 1
           if (not ndev_rack):
               nb_non_existent_objects['rack'].append(row['rack'])
               nb_non_existent_count += 1
 
           # Generates dict of values for PyNetbox to create object
-          nb_all_devices.append(
-            dict(
-                name=row['name'],
-                site=ndev_site,
-                device_type=ndev_dtype.id,
-                device_role=ndev_drole.id,
-                rack=ndev_rack.id,
-                face=row['face'],
-                position=row['position'],
-                serial=row['serial'],
-                asset_tag=row['asset_tag'],
-                status=row['status'],
-            )
-          )
+          if (nb_non_existent_count == 0):
+              nb_all_devices.append(
+                dict(
+                    name=row['name'],
+                    site=ndev_site.id,
+                    device_type=ndev_dtype.id,
+                    device_role=ndev_drole.id,
+                    rack=ndev_rack.id,
+                    face=row['face'],
+                    position=row['position'],
+                    serial=row['serial'],
+                    asset_tag=row['asset_tag'],
+                    status=row['status'],
+                )
+              )
 
-          nb_all_devices_primaryIPs[row['name']] = row['primary_ipv4']
+              nb_all_devices_primaryIPs[row['name']] = row['primary_ipv4']
+              all_IPs.append(row['primary_ipv4'])
 
 except FileNotFoundError as e:
     print(e)
 except pynetbox.core.query.RequestError as e:
     print(e.error)
 
+flag = len(set(all_IPs)) == len(all_IPs)
+
 ### Generates table of non-existent NetBox objects defined in CSV
-if (nb_non_existent_count > 0):
+if ( (nb_non_existent_count > 0) or not(flag) ):
+    # Print results of verifying duplicated IPs
+    if(not flag):
+        print()
+        print(12*"*"," Verify for duplicated IPs ",12*"*")
+        print ("One or more of the devices have duplicated IPs")
+
+    print()
     print(12*"*"," Verify the following NetBox Objects ",12*"*")
     print(fmt.format(*header))
 
@@ -96,73 +110,55 @@ if (nb_non_existent_count > 0):
                     "Non-Existent"
                 )
             )
+else:
+    try:
+        # Retrieve device object by name and delete
+        for dev in nb_all_devices:
+            dev = nb.dcim.devices.get(name=dev['name'])
+            dev.delete()
 
-pprint.pprint(nb_all_devices)
-pprint.pprint(nb_all_devices_primaryIPs)
+        # Add devices to NetBox and store resulting object in "created_devs"
+        nb_created_devices = nb.dcim.devices.create(nb_all_devices)
 
+        # Formatting and header for output
+        fmt = "{:<15}{:<20}{:<15}{:<10}{:<15}{:<25}{:<20}"
+        header = ("Device", "Dev Role", "Dev Type", "Site", "Rack", "Management Interface", "IP")
+        print()
+        print(50*"*"," Created Devices ",50*"*")
+        print(fmt.format(*header))
 
+        for created_dev in nb_created_devices:
+            # Retrieve specific interface associated w/ created device
+            nb_primary_interface = nb.dcim.interfaces.filter(device=created_dev.name,name="FastEthernet0/0")
 
-# ### Creating Spine and Leaf devices
-#
-# SPINE_NUM = 2
-# LEAF_NUM = 4
-#
-# # List to store dictionary attributes
-# ndev_list = list()
-#
-# ndev_site = nb.dcim.sites.get(slug="hq1")
-# ndev_dtype = nb.dcim.device_types.get(slug="7200-series")
-# ndev_drole_spine = nb.dcim.device_roles.get(slug="network-core")
-# ndev_drole_leaf = nb.dcim.device_roles.get(slug="network-access")
-#
-# # Generate spine attributes
-#
-# for i in range(1,SPINE_NUM+1):
-#     ndev_list.append(
-#         dict(
-#             name="sw-spine-0{swid}".format(swid=i),
-#             device_type=ndev_dtype.id,
-#             device_role=ndev_drole_spine.id,
-#             site=ndev_site.id,
-#         )
-#     )
-#
-# # Generate leaf attributes
-#
-# for i in range(1,LEAF_NUM+1):
-#     ndev_list.append(
-#         dict(
-#             name="sw-leaf-0{swid}".format(swid=i),
-#             device_type=ndev_dtype.id,
-#             device_role=ndev_drole_leaf.id,
-#             site=ndev_site.id,
-#         )
-#     )
-#
-# try:
-#
-#     # Retrieve device object by name and delete
-#     for dev in ndev_list:
-#         nb_dev = nb.dcim.devices.get(name=dev['name'])
-#         nb_dev.delete()
-#
-#     results = nb.dcim.devices.create(ndev_list)
-#
-#     # Formatting and header for output
-#     fmt = "{:<25}{:<25}{:<25}{:<15}"
-#     header = ("Device", "Dev Role", "Dev Type", "Site")
-#     print(fmt.format(*header))
-#
-#     # Print summary info for each created device
-#     for r in results:
-#         print(
-#             fmt.format(
-#                 r.name,
-#                 r.device_role.name,
-#                 r.device_type.model,
-#                 r.site.name,
-#             )
-#         )
-#
-# except pynetbox.core.query.RequestError as e:
-#     print(e.error)
+            # Create dict to store attributes for device's primary IP
+            primary_ip_addr_dict = dict(
+                address=nb_all_devices_primaryIPs[created_dev.name],
+                status=1,
+                description="Management IP for {}".format(created_dev.name),
+                interface=nb_primary_interface[0].id,
+            )
+
+            # Create primary IP and assign to device's first interface
+            new_primary_ip = nb.ipam.ip_addresses.create(primary_ip_addr_dict)
+
+            # Retrieves created device, and sets the primary IP for the device
+            dev = nb.dcim.devices.get(created_dev.id)
+            dev.primary_ip4 = new_primary_ip.id
+            dev.save()
+
+            # Print summary info for each created device
+            print(
+                fmt.format(
+                    created_dev.name,
+                    created_dev.device_role.name,
+                    created_dev.device_type.model,
+                    created_dev.site.name,
+                    created_dev.rack.name,
+                    nb_primary_interface[0].name,
+                    new_primary_ip.address
+                )
+            )
+
+    except pynetbox.core.query.RequestError as e:
+        print(e.error)
